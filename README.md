60acf94b7a7ca1da153fca7037c72c89ddb6c7cb
Dez384 <164457313+Dez384@users.noreply.github.com>
2024-11-24 21:58:21 -0500

    Special Status Effects
    Added the other five Foundry Special Status Effects

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Cosmere Useful Macros
 This module is for use in the currently open Beta test of the Cosmere RPG. It's purpose is to provide macros for ease of access to certain features while the core system is still being built.
 * This module contains the following world scripts:
-  *  Adds the *Invisible* condition to the game system to enable certain core Foundry features.
+  *  Adds the following special condition to the game system to enable certain core Foundry features:
+    * Invisible, Blind, Burrow, Defeated, Fly, and Hover
+    * Each condition can be enabled or disabled individually in the Settings menu
 * The following macros are currently included:
   * __Sphere Lantern Picker__: Automates setting a selected token's light source using a sphere lantern by fulness and color
   * __Toggle Invisibility__: Adds or Removes the *Invisible* condition from all selected tokens
--- world.js
+++ world.js
@@ -1,18 +1,74 @@
-Hooks.once("init", () => {
-  const effects = {
-    invisible: {
-      label: "invisible",
-      icon: "icons/svg/invisible.svg"
-    }
-  };
+// new conditions object; key=condition name; value=icon name
+	const NewCon = {
+		"invisible" : "invisible",
+		"blind" : "blind",
+		"burrow" : "mole",
+		"defeated": "skull",
+		"fly" : "wing",
+		"hover" : "wingfoot"
+	};
 
-  for (const [k, v] of Object.entries(effects)) {
-    CONFIG.statusEffects.push({
-      id: k,
-      _id: `cond${k}`.padEnd(16, '0'),
-      name: v.label,
-      img: v.icon
-    });
-	CONFIG.COSMERE.conditions[k] = {label: v.label, icon: v.icon};
-  }
-});
+// Init Hook
+Hooks.once("init", async () => {
+
+	// Register Settings for new conditions
+		await registerConditionSettings();
+	// get list of new conditions and add them
+		await addNewConditions();
+}); //end init hook
+
+
+// function for registering all the additional condition settings
+async function registerConditionSettings() {
+	let allSettings = Object.keys(NewCon);
+	allSettings.forEach ((i)=>{
+		let C = i.charAt(0).toUpperCase()+i.slice(1);
+		console.log(C);
+		game.settings.register('cosmere-useful-macros', `condition-${i}`, {
+			name: `${C} Condition`,
+			hint: `Enables the ${C} Condition.`,
+			scope: 'world',
+			config: true,
+			type: new foundry.data.fields.BooleanField(),
+			requiresReload: true
+			}
+		);	
+	});
+};
+
+async function addNewConditions() {
+
+	let newConditions = [];
+	let conditionList = Object.keys(NewCon);
+	conditionList.forEach((i)=> {
+		if(game.settings.get('cosmere-useful-macros','condition-'+i)) {
+		newConditions.push(i);
+		}
+	});	
+	console.log("New Conditions are: "+newConditions);
+
+	newConditions.forEach((i)=> {
+		console.log("Current effect: "+i); 
+		let capLabel = i.charAt(0).toUpperCase()+i.slice(1);
+		let effects = {
+			[i] : {
+				label: capLabel,
+				icon: "icons/svg/"+NewCon[i]+".svg"
+			}
+		};
+		
+		console.log(effects);
+
+		for (const [k, v] of Object.entries(effects)) {
+			CONFIG.statusEffects.push({
+			  id: k,
+			  _id: `cond${k}`.padEnd(16, '0'),
+			  name: v.label,
+			  img: v.icon
+			});
+		CONFIG.COSMERE.conditions[k] = {label: v.label, icon: v.icon};
+	  };
+	  
+	});
+
+};
